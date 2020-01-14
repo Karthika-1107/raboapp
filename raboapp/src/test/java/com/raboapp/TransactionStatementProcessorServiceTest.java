0c06cb3b1f8d23fb44fd5174143b414ceed3b511
Karthika-1107 <59686705+Karthika-1107@users.noreply.github.com>
2020-01-15 00:12:08 +0530

    junit

--- raboapp/src/test/java/com/raboapp/TransactionStatementProcessorServiceTest.java
+++ raboapp/src/test/java/com/raboapp/TransactionStatementProcessorServiceTest.java
@@ -3,16 +3,18 @@ package com.raboapp;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.raboapp.bean.TransactionDetail;
 import com.raboapp.service.TransactionStatementProcessorService;
 
-
 import static org.junit.Assert.assertEquals;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class TransactionStatementProcessorServiceTest {
 	@Autowired
