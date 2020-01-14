15b8db443442864855fa79e3ad748bc6a1b89ecc
Karthika-1107 <59686705+Karthika-1107@users.noreply.github.com>
2020-01-15 00:10:37 +0530

    junit

--- raboapp/src/main/java/com/raboapp/bean/TransactionDetail.java
+++ raboapp/src/main/java/com/raboapp/bean/TransactionDetail.java
@@ -2,7 +2,9 @@ package com.raboapp.bean;
 
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement(name="record")
 public class TransactionDetail {
 	private String refId;
 	private String iban;
@@ -11,52 +13,46 @@ public class TransactionDetail {
 	private String endBalance;
 	private String mutation;
 	
-	@XmlAttribute(name="Reference")
+	@XmlAttribute(name="reference")
 	public String getRefId() {
 		return refId;
 	}
 	public void setRefId(String refId) {
 		this.refId = refId;
 	}
-	@XmlElement(name="Account Number")
+	@XmlElement(name="accountNumber")
 	public String getIban() {
 		return iban;
 	}
 	public void setIban(String iban) {
 		this.iban = iban;
 	}
-	@XmlElement(name="Description")
+	@XmlElement(name="description")
 	public String getDescription() {
 		return description;
 	}
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	@XmlElement(name="Start Balance")
+	@XmlElement(name="startBalance")
 	public String getStartBalance() {
 		return startBalance;
 	}
 	public void setStartBalance(String startBalance) {
 		this.startBalance = startBalance;
 	}
-	@XmlElement(name="End Balance")
+	@XmlElement(name="endBalance")
 	public String getEndBalance() {
 		return endBalance;
 	}
 	public void setEndBalance(String endBalance) {
 		this.endBalance = endBalance;
 	}
-	@XmlElement(name="Mutation")
+	@XmlElement(name="mutation")
 	public String getMutation() {
 		return mutation;
 	}
 	public void setMutation(String mutation) {
 		this.mutation = mutation;
 	}
-	
-	@Override
-	public String toString() {
-		return refId;
-	}
-
 }
